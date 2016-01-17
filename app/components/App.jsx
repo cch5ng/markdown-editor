1e7cbfc85339fcba27b5ce80bddd3f9ab06843c1
cch5ng <cch5ng@gmail.com>
2016-01-17 13:41:46 -0800

    fix: markdown editor

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,23 +3,19 @@
 import React from 'react';
 // import Editor from './Editor.jsx';
 // import Preview from './Preview.jsx';
+var marked = require('marked');
 
 //TODO see if I can 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {content: props.initialContent, markdown: props.initialMd};
-		/*this.setState = this.setState.bind(this);*/
 	}
 
-	// getInitialState() {
-	// 	return {value: 'Hello!'};
-	// }
-
 	handleChange(event) {
-		console.log('value: ' + event.target.value);
-		this.setState({content: event.target.value});
-		//console.log({this.state.content});
+		var md = marked(event.target.value);
+		this.setState({content: event.target.value, markdown: md});
+		$('#preview_iframe').contents().find('html').html(this.state.markdown);
 	}
 
 	render() {
@@ -28,14 +24,17 @@ export default class App extends React.Component {
 			<div className="container">
 				<div className="row gap-medium"></div>
 				<div className="row">
-					{/*<Editor />*/}
 					<div className="col-xs-6 col-sm-6">
+						<p className="h2">Markdown Editor <span className="link"><a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet" target="_blank">(cheatsheet)</a></span></p>
 						{/* had to bind to this in the onChange call to handleChange() */}
 						<textarea cols="70" rows="25" className="editor" value={this.state.content} onChange={this.handleChange.bind(this)}></textarea>
 					</div>
 
-					{/*<Preview />*/}
-					<div className="col-xs-6 col-sm-6">preview {this.state.markdown}</div>
+					<div className="col-xs-6 col-sm-6">
+						<h2>Preview</h2>
+						<iframe id="preview_iframe" height="505" seamless="true">
+						</iframe>
+					</div>
 
 				</div>
 			</div>
@@ -43,5 +42,7 @@ export default class App extends React.Component {
 	}
 }
 
+var initMarkdown = marked('# Heading1');
+console.log('initMarkdown: ' + initMarkdown);
 App.propTypes = { initialContent: React.PropTypes.string };
-App.defaultProps = { initialContent: '# Heading1 ## Heading2', initialMd: 'initial markdown'};
+App.defaultProps = { initialContent: '', initialMd: ''};
