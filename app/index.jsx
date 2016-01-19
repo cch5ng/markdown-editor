b37791350085c39bf76a5e4645f23d1605b21d18
cch5ng <cch5ng@gmail.com>
2016-01-18 20:12:55 -0800

    fix: update app to properly require the sass, css, style loaders build the bundle.js with css

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,14 @@
 //app/index.jsx
 
-import './main.css';
+//import './main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-//var css = require("!css!sass!./main.scss");
+require("!style!css!sass!./main.scss");
+
+//var css = require("!raw!sass!./file.scss");
+var css = require("!css!sass!./main.scss");
 
 ReactDOM.render(<App />, document.getElementById('app'));
